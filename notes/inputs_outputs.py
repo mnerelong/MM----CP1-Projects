12ca545a07ff55bdbc03077e4907ffc0979203fd
mnerelong <meika.milton@ucas-edu.net>
2026-08-19 09:19:01 -0600

    organized files and added notes to inputs_outputs

--- notes/inputs_outputs.py
+++ notes/inputs_outputs.py
@@ -13,3 +13,10 @@ print(f"Wait, did you say that your name is {name}? I've heard of you!!\n")
 print("How do you feel, on a scale of 1 to 10?")
 doing = input()
 print(f"{doing}? Heh, whatever. I didn't even ask. I don't even care!!!")
+
+
+# input - what the user puts in.
+#   ex : input(I want an input from the user)
+
+# output - what the program gives back to the user.
+#   ex : print(this is an output)
