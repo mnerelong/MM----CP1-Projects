df4c76507dfb2b58396cde06fe4be6331421cf4c
mnerelong <meika.milton@ucas-edu.net>
2026-09-01 09:02:31 -0600

    data types notes 912026

---
+++ notes/data_types_notes.py
@@ -0,0 +1,21 @@
+# Meika Milton, Data Types Notes
+#integers. whole numbers.
+integer = 1
+#         ^ this is an integer. just write the number.
+not_integer = 1.00
+#              ^ this is a float, because of the decimal.
+
+# Arithmetic Operators (+ (add), - (subtract), * (multiply), / (division), ** (exponents), // (int. division), % (modulo))
+print(5 / 2) # Regular division always gives us a float.
+print(5 // 2) # No decimal/ Gives us an integer.
+
+print(9 % 4) # Modulo returns the leftover of division. *the remainder of a division problem
+# Modulo/Mod | % is called a modulus
+
+print(231/2+4-1%3**5)
+# Order of operations "PEMMDAS"
+#   Parenthesis, exponents, multiplication uh or modulo or both, division, addition, subtraction.
+
+# Assignment Operator -> =
+variable = 23
+variable += 21
