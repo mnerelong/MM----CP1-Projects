5cf75303804394afb6e3afa8954f6233e2d1f250
mnerelong <meika.milton@ucas-edu.net>
2026-09-11 02:27:18 +0000

    finished dice roller

--- projects/dice_roller.py
+++ projects/dice_roller.py
@@ -2,10 +2,12 @@
 import random
 
 def roll(list):
+    total = 0
     for max in list:
         local_result = random.randint(1, max)
+        total += local_result
         print(f"The D{max} rolled {local_result}.")
-    return local_result
+    return total
 
 pick_dice = ["D4", "D6", "D8", "D10", "D12", "D20", "D100"] # meet potential dice
 chosen_dice = []
@@ -45,10 +47,11 @@ while True: #  okay.... are nested loops alright?
     loop = input(f"You've rolled {loop_num} times. Roll? (yes/no)\n").lower()
     if loop == "yes":
         print("\n") # skips
-        results += roll(roll_max)
+        results = roll(roll_max)
         print(f"Your total is {results}!!")
         loop_num += 1
     elif loop != "yes" and loop_num == 0:
-        print("You haven't even rolled? Are you sure?")
+        print("You haven't even rolled? Are you sure?\n(You have no other choice)")
     else:
         print(f"Thanks for using my dice. You're going to have to pay for that.\nIt's gonna cost you about ${results}.")
+        break
