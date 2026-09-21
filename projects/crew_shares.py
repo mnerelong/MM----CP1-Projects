1c8ff400be489a97fc51ce10f7f1b05332f3da6c
mnerelong <meika.milton@ucas-edu.net>
2026-09-21 09:42:39 -0600

    finished crew shares?

---
+++ projects/crew_shares.py
@@ -0,0 +1,50 @@
+# Meika Milton, 1st period Programming I, Crew Shares
+
+import random
+
+def udonta_quill(start_amt, person, amount):
+    person = round((amount/100) * start_amt, 2)
+    return person
+
+yondu_share = 0
+peter_share = 0
+crew_shares = 0
+
+while True:
+    try:
+        pirate_total = int(input("How many pirates are there?\n"))
+    except:
+        print("?? Weird crew size, huh?")
+    else:
+        if pirate_total <= 2:
+            print("Sorry... yeah... there's more than that actually.")
+        else:
+            break
+
+pirate_names = []
+
+for pirate in range(1, (pirate_total - 1)):
+    pirate_names.append(input(f"Whats the name of pirate # {pirate}?\n").strip().title())
+
+initial_plunder = random.randint(500, 5001)
+plunder = initial_plunder
+
+crew_shares = 3
+plunder -= crew_shares * pirate_total
+
+yondu_share = udonta_quill(plunder, yondu_share, 13)
+peter_share = udonta_quill(plunder, peter_share, 11)
+plunder -= yondu_share + peter_share
+
+equal_share = round(plunder / pirate_total, 2)
+
+yondu_share = round(equal_share + yondu_share, 2)
+peter_share = round(equal_share + peter_share, 2)
+crew_shares = round(equal_share + crew_shares, 2)
+
+
+print(f"\nThe crew's total unit plunder was {initial_plunder}!\n")
+print(f"Yondu has {yondu_share} units!\nPeter has {peter_share} units!")
+
+for member in pirate_names:
+    print(f"{member} has {crew_shares} units!")
