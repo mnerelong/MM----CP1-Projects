34ff169bf99ed086249b8e1c8122856b3d0cd72a
mnerelong <meika.milton@ucas-edu.net>
2026-09-22 09:28:16 -0600

    fixed crew shares i think. added a pirate limit

--- projects/crew_shares.py
+++ projects/crew_shares.py
@@ -12,7 +12,7 @@ crew_shares = 0
 
 while True:
     try:
-        pirate_total = int(input("How many pirates are there?\nINCLUDING Yondu and Peter.\nAlso you have to name all of them.\n"))
+        pirate_total = int(input("How many pirates are there (including Yondu and Peter)?"))
     except:
         print("?? Weird crew size, huh?")
     else:
@@ -21,12 +21,16 @@ while True:
         else:
             break
 
-pirate_names = []
+full_print = 0 # 0 for false.
 
-for pirate in range(1, (pirate_total - 1)):
-    pirate_names.append(input(f"Whats the name of pirate # {pirate}?\n").strip().title())
+if pirate_total <= 25:
+    pirate_names = []
+    full_print = 1 # 1 for true.
 
-initial_plunder = random.randint(500, 5000) # i did 5001 because i thought that the 2nd number isnt included and i wanted to include 5000 im sorry
+    for pirate in range(1, (pirate_total - 1)): # only -1 because it's already not including the last number
+        pirate_names.append(input(f"Whats the name of pirate # {pirate}?\n").strip().title())
+
+initial_plunder = random.randint(500, 5000) # i did 5001 because i thought that the 2nd number wasnt included and i wanted to include 5000 im sorry
 plunder = initial_plunder
 
 crew_shares = 3
@@ -43,11 +47,14 @@ equal_share = round(plunder / pirate_total, 2)
 
 yondu_share = round(equal_share + yondu_share, 2)
 peter_share = round(equal_share + peter_share, 2)
-crew_shares = round(equal_share + crew_shares, 2)
+crew_shares = round(equal_share, 2)
 
 
 print(f"\nThe crew's total unit plunder was {initial_plunder}!\n")
 print(f"Yondu has {yondu_share} units!\nPeter has {peter_share} units!")
 
-for member in pirate_names:
-    print(f"{member} has {crew_shares} units!")
+if full_print == 1:
+    for member in pirate_names:
+        print(f"{member} has {crew_shares} units!")
+else:
+    print(f"The rest of the crew has {crew_shares} units!")
